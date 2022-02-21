a1c46ff17289adad44e96f1fff7c198f4bdd2ed2
taojoue <taojouet@gmail.com>
2022-02-21 15:41:23 +0100

    commit request back

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-const db = require('./queries')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const db = require('./queries');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-)
+);
 
 app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+    response.json({ info: 'Node.js, Express, and Postgres API' });
+});
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+    console.log(`App running on port ${port}.`);
+});
 
-app.get('/users', db.getUsers)
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.get('/updateUsers/:name', db.updateUsers);
--- src/queries.js
+++ src/queries.js
@@ -1,21 +1,67 @@
-const Pool = require('pg').Pool
+// import { Pool } from "pg";
+
+
+const Pool = require('pg-pool');
 const pool = new Pool({
-    user: 'postgre',
+    user: 'postgres',
     host: 're-source-1-instance-1.c9itwz8vqblz.us-east-1.rds.amazonaws.com',
-    database: 're-source-1',
+    database: 'Re(Sources)',
     password: 'ressource',
     port: 5432,
-})
+    // connectionString: "postgres://postegres:ressource@re-source-1-instance-1.c9itwz8vqblz.us-east-1.rds.amazonaws.com",
+    // ssl: {
+    //     rejectUnauthorized: false
+    // }
+});
 
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM "Compte"', (error, results) => {
         if (error) {
             throw error
         }
-        response.status(200).json(results.rows)
+        response.status(200).json(results.rows);
+    })
+}
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    pool.query('SELECT * FROM "Compte" WHERE "idCompte" = $1', [id], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        // response.status(200).json(results.rows);
+        // var reponse = Response.redirect(url,status);
+        reponse.redirect('/users', 200);
+    })
+}
+
+const createUser = (request, response) => {
+    const { name, email } = request.body
+
+    pool.query('INSERT INTO "Compte" (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(201).send(`User added with ID: ${result.insertId}`)
+    })
+}
+
+const updateUsers = (request, response) => {
+    const { name } = request.body;
+    console.log(name);
+    pool.query('INSERT INTO "Compte" ("nomUtilisateur") VALUES ($1)', [name], (error, results) => {
+        if (error) {
+            throw error
+        }        
+        // response.status(200).json(results.rows);
+        response.redirect('/users');
     })
 }
 
 module.exports = {
     getUsers,
+    getUserById,
+    createUser,
+    updateUsers
 }
